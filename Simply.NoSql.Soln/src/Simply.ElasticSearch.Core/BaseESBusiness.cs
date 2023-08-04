d7292766b09403a73c38cd5a22692a5fa66e5693
mustafasacli <mustasacli@hotmail.com>
2023-08-04 11:31:13 +0300

    base files

---
+++ Simply.NoSql.Soln/src/Simply.ElasticSearch.Core/BaseESBusiness.cs
@@ -0,0 +1,112 @@
+﻿using Nest;
+using System;
+
+namespace ErrorLog.Business.ElasticSearch.Core
+{
+    /// <summary>
+    /// Base Elastic Search Business.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class BaseESBusiness<T> where T : class, new()
+    {
+        /// <summary>
+        /// Gets, sets ElasticClient.
+        /// </summary>
+        protected ElasticClient Client { get; set; }
+
+        /// <summary>
+        /// Gets uri.
+        /// </summary>
+        public string Uri
+        { get; protected set; }
+
+        /// <summary>
+        /// Gets index name.
+        /// </summary>
+        public string IndexName
+        { get; protected set; }
+
+        /// <summary>
+        /// protected base elastic search business ctor.
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <param name="indexName"></param>
+        protected BaseESBusiness(string uri, string indexName)
+        {
+            if (string.IsNullOrWhiteSpace(uri))
+                throw new ArgumentNullException(nameof(uri));
+
+            if (string.IsNullOrWhiteSpace(indexName))
+                throw new ArgumentNullException(nameof(indexName));
+
+            this.Uri = uri;
+            this.IndexName = indexName;
+            Uri node = new Uri(this.Uri);
+            ConnectionSettings settings = new ConnectionSettings(node);
+            this.Client = new ElasticClient(settings);
+        }
+
+        /// <summary>
+        /// Checks Index, if not exist create.
+        /// </summary>
+        protected virtual void CheckIndex()
+        {
+            if (!this.Client.Indices.Exists(this.IndexName).Exists)
+            {
+                this.Client.Indices.Create(this.IndexName, idx => idx.Index(this.IndexName).Map<T>(q => q.AutoMap()));
+            }
+        }
+
+        /// <summary>
+        /// Insert and return id of model.
+        /// </summary>
+        /// <param name="log"></param>
+        /// <returns></returns>
+        public string CheckExistsAndInsert(T log)
+        {
+            CheckIndex();
+            // TODO : Elastic Search Result nesnesi için uygun geri dönüş modeli, oluşturulacak.
+            IndexResponse response = this.Client.Index(log, idx => idx.Index(this.IndexName));
+            string result = response.Id;
+            return result;
+        }
+
+        /// <summary>
+        /// Gets model by with given id.
+        /// </summary>
+        /// <param name="oid"></param>
+        /// <returns></returns>
+        public T Get(string oid)
+        {
+            CheckIndex();
+            T model = new T();
+
+            if (!string.IsNullOrWhiteSpace(oid))
+            {
+                GetResponse<T> response = this.Client.Get<T>(oid, idx => idx.Index(this.IndexName));
+                model = response.Source;
+            }
+
+            return model;
+        }
+
+        /// <summary>
+        /// Error = 0, Created = 1, Updated = 2, Deleted = 3, NotFound = 4, Noop = 5
+        /// </summary>
+        /// <param name="oid"></param>
+        /// <returns></returns>
+        public int Delete(string oid)
+        {
+            CheckIndex();
+            var result = -10;
+
+            if (!string.IsNullOrWhiteSpace(oid))
+            {
+                var response = this.Client.Delete<T>(oid, idx => idx.Index(this.IndexName));
+                result = (int)response.Result;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Simply.NoSql.Soln/src/Simply.RavenDb.Core/RavenDbDocumentStore.cs
@@ -0,0 +1,80 @@
+﻿namespace Mst.RavenDb.Core
+{
+    using Raven.Client;
+    using Raven.Client.Document;
+    using System;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// <summary>   A raven database document store. </summary>
+    ///
+    /// <remarks>   Msacli, 24.04.2019. </remarks>
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    public class RavenDbDocumentStore
+    {
+        private static object lockObj = new object();
+        private IDocumentStore docStore = null;
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Constructor. </summary>
+        ///
+        /// <remarks>   Msacli, 24.04.2019. </remarks>
+        ///
+        /// <exception cref="ArgumentNullException">    Thrown when one or more required arguments are null. </exception>
+        ///
+        /// <param name="defaultDatabaseName">  Gets Default Database Name. </param>
+        /// <param name="dbServerUrl">          Gets Db Server Url. </param>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        public RavenDbDocumentStore(string defaultDatabaseName, string dbServerUrl)
+        {
+            if (string.IsNullOrWhiteSpace(defaultDatabaseName))
+                throw new ArgumentNullException(nameof(defaultDatabaseName));
+
+            if (string.IsNullOrWhiteSpace(dbServerUrl))
+                throw new ArgumentNullException(nameof(dbServerUrl));
+
+            this.DefaultDatabaseName = defaultDatabaseName;
+            this.DbServerUrl = dbServerUrl;
+        }
+
+        /// <summary>
+        /// gets Db Server Url
+        /// </summary>
+        public string DbServerUrl
+        { get; private set; }
+
+        /// <summary>
+        /// gets Default Database Name
+        /// </summary>
+        public string DefaultDatabaseName
+        { get; private set; }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>   Gets the document store. </summary>
+        ///
+        /// <value> The document store. </value>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        public IDocumentStore DocumentStore
+        {
+            get
+            {
+                if (docStore == null)
+                {
+                    lock (lockObj)
+                    {
+                        if (docStore == null)
+                        {
+                            var docStoreInstance = new DocumentStore();
+                            docStoreInstance.Url = this.DbServerUrl;
+                            docStoreInstance.DefaultDatabase = this.DefaultDatabaseName;
+
+                            docStoreInstance.Initialize();
+                            docStore = docStoreInstance;
+                        }
+                    }
+                }
+
+                return docStore;
+            }
+        }
+    }
+}
