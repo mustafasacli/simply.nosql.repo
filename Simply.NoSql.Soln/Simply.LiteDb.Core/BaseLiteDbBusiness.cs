dc4d6b161b284c9aa9f12ee8c7d171bcba00aa2a
mustafasacli <mustasacli@hotmail.com>
2023-08-04 13:14:13 +0300

    Lite db developments.

---
+++ Simply.NoSql.Soln/Simply.LiteDb.Core/BaseLiteDbBusiness.cs
@@ -0,0 +1,40 @@
+﻿using LiteDB;
+using System;
+
+namespace Simply.LiteDb.Core
+{
+    /// <summary>
+    /// The base lite db business.
+    /// </summary>
+    public abstract class BaseLiteDbBusiness<T> : IDisposable where T : class
+    {
+        protected LiteDatabase database = null;
+        protected readonly ILiteCollection<T> liteCollection;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseLiteDbBusiness"/> class.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <param name="mapper">The mapper.</param>
+        protected BaseLiteDbBusiness(string connectionString, BsonMapper mapper = null)
+        {
+            database = new LiteDatabase(connectionString, mapper);
+            Collection = database.GetCollection<T>(typeof(T).Name.ToLowerInvariant());
+            liteCollection = database.GetCollection<T>(typeof(T).Name.ToLowerInvariant());
+        }
+
+        /// <summary>
+        /// Gets or sets the collection.
+        /// </summary>
+        public ILiteCollection<T> Collection
+        { get; protected set; }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            database?.Dispose();
+        }
+    }
+}
