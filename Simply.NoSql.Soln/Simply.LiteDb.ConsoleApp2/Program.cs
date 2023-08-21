9be3ed0349e5aab0595402bf58fdff049d0cf075
mustafasacli <mustasacli@hotmail.com>
2023-08-22 00:26:36 +0300

    yeni proje eklendi.

---
+++ Simply.NoSql.Soln/Simply.LiteDb.ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using Simply.LiteDb.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simply.LiteDb.ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (LiteDbBusiness business = new LiteDbBusiness())
+            {
+                var data = business.GetByParameters().ToList();
+                Console.WriteLine("Veri sayısı: " + data.Count);
+                for (int counter = 0; counter < data.Count; counter++)
+                {
+                    var model = data[counter];
+                    int deleteResult = business.Delete(model.OId);
+                    Console.WriteLine($"{model.OId} id ile silinme sonucu: {deleteResult}.");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
